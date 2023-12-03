83f0b6dcd1ecb56e70f124f5176d0780392e9aef
20ele <20elenatashlykova08@gmail.com>
2023-12-03 17:49:04 +0500

    eche nemnogo

--- src/level.py
+++ src/level.py
@@ -1,14 +1,14 @@
 import pygame
 from settings import *
 from tile import Tile
-from player import Player
+from playerr import Player
 
 
 class Level:
     def __init__(self):
         self.screen = pygame.display.get_surface()
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         self.create_map()
 
@@ -18,7 +18,9 @@ class Level:
                 # tilesize = 64
                 x, y = col_index * tilesize, row_index * tilesize
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
+                if col == 'p':
+                    Player((x, y), [self.visible_sprites])
 
     def run(self):
         self.visible_sprites.draw(self.screen)
--- src/playerr.py
+++ src/playerr.py
@@ -1,8 +1,8 @@
 import pygame
 
 
-class PLayer(pygame.sprite.Sprite):
+class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
         self.image = pygame.image.load('../1-level/graphics/test/player.png').convert_alpha()
-        self.rect = self.get_rect(topleft=pos)
+        self.rect = self.image.get_rect(topleft=pos)
